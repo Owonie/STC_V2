28857155ffb6398e80b1133e1fea4334f26853e8
Owon <chlehdnjs@gmail.com>
2023-10-31 14:24:06 +0900

    chore: firebase setup

---
+++ src/services/firebase.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
+};
+const firebaseApp = initializeApp(firebaseConfig);
+const db = getFirestore(firebaseApp);
+
+export { firebaseApp, db };
