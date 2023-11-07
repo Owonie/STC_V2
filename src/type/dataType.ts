df90dc0671b6e13d6671643afb05b095a49edac0
Owon <chlehdnjs@gmail.com>
2023-11-07 13:11:45 +0900

    create: dataType.ts

---
+++ src/type/dataType.ts
@@ -0,0 +1,15 @@
+// account data
+export type User = {
+  displayName: string;
+  photoURL: string;
+  accessToken: string;
+};
+
+// room data
+export type CurrentRoomServer = {
+  isInRoomServer: boolean;
+};
+
+export type RoomServer = {
+  roomServerName: string;
+};
